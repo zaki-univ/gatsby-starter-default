fde80403e45b4dff973a476d3d765f336b821c5d
James Kerrane <hi@thatrobot.dev>
2019-10-14 14:38:21 -0600

    Update broken link
    The link gatsby.js/offline was broken, so I changed it to the link for gatsby-plugin-offline docs.

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
+    // To learn more, visit: https://www.gatsbyjs.org/docs/add-offline-support-with-a-service-worker/
     // `gatsby-plugin-offline`,
   ],
 }
